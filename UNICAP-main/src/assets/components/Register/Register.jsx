b24aa033d77c465f5fe82f1901b686af06b45223
Alex-cavalcanti <111925066+Alex-cavalcanti@users.noreply.github.com>
2024-06-03 18:08:15 -0300

    Update Register.jsx

--- UNICAP-main/src/assets/components/Register/Register.jsx
+++ UNICAP-main/src/assets/components/Register/Register.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import "../Register/Register.css"; 
 
 function RegisterUser() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
-  const [cargo, setCargo] = useState(1); // Assume que o cargo padrão é 1
+  const [cargo, setCargo] = useState(1);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleGoToLogin = () => {
+    navigate('/login');
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setError('');
     const userData = {
       nome,
       email,
@@ -17,44 +28,61 @@ function RegisterUser() {
     try {
         const response = await axios.post('https://localhost:44365/api/User/CreateUser', userData);
         console.log('Usuário criado com sucesso:', response.data);
-      alert('Usuário criado com sucesso!');
+        alert('Usuário criado com sucesso!');
+        navigate('/login'); 
     } catch (error) {
       console.error('Erro ao criar usuário:', error);
-      alert('Erro ao criar usuário!');
+      setError('Erro ao criar usuário. Por favor, tente novamente.');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Nome:</label>
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          required
-        />
+    <div className="register-container">
+      <div className="register-box">
+        <h1 className="register-title">Registro</h1>
+        {error && <p className="register-error-message">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <div className="register-form-group">
+            <label className="register-content" htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-form-group">
+            <label className="register-content" htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-form-group">
+            <label className="register-content" htmlFor="cargo">Cargo</label>
+            <input
+              type="number"
+              id="cargo"
+              value={cargo}
+              onChange={(e) => setCargo(e.target.value)}
+              required
+            />
+          </div>
+          <div className="register-form-group">
+            <button type="button" className="register-button" onClick={handleGoToLogin}>Voltar ao Login</button>
+            <button type="submit" className="register-button" disabled={loading}>
+              {loading ? "Carregando..." : "Registrar Usuário"}
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Cargo:</label>
-        <input
-          type="number"
-          value={cargo}
-          onChange={(e) => setCargo(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Registrar Usuário</button>
-    </form>
+    </div>
   );
 }
 
